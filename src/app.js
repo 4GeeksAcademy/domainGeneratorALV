a8ce68adc7888d5d35c66a7efe37caf66e422e05
Omar Gutierrez Sanchez <117556558+spirosis@users.noreply.github.com>
2023-10-05 00:18:19 +0000

    my domain name generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let prefix = ["your", "the"];
+let pron = ["olympus", "granero"];
+let adj = ["mamalon", "chingon", "bergas"];
+let domain = [".com", ".org", ".mx", ".alv"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let domainName = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function GenerateDomain() {
+  for (let i = 0; i < prefix.length; i++) {
+    for (let j = 0; j < pron.length; j++) {
+      for (let k = 0; k < adj.length; k++) {
+        for (let p = 0; p < domain.length; p++) {
+          let div = document.createElement("div");
+          div.innerText = `
+                    ${prefix[i]}${pron[j]}${adj[k]}${domain[p]}
+                    `;
+          document.body.appendChild(div);
+        }
+      }
+    }
+  }
+}
+window.onload = GenerateDomain();
